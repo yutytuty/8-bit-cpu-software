293b81d9ec3e1c3a1ca5dcbd37a341d9eb3d5c4e
yutytuty <49990984+yutytuty@users.noreply.github.com>
2023-08-23 02:51:06 +0300

    Add loading image into ROM

--- emu/emu.c
+++ emu/emu.c
@@ -3,7 +3,22 @@
 
 #include "emu.h"
 
+static void load_image(struct CPU* state, FILE* image) {
+  // Determine file size
+  fseek(image, 0, SEEK_END);
+  long image_size = ftell(image);
+  fseek(image, 0, SEEK_SET);
+  if (image_size > ROM_SIZE) {
+    warn("Image to large for ROM. last %d bytes not loaded.", image_size - ROM_SIZE);
+    image_size = ROM_SIZE;
+  }
+  // Read the file into rom
+  unsigned long bytes_read = fread(state->memory.rom, 1, image_size, image);
+}
+
 int main(int argc, char *argv[]) {
+  struct CPU state;
+
   if (argc != 2) {
     printf("Usage: %s <image file>\n", argv[0]);
     return EXIT_FAILURE;
@@ -11,11 +26,12 @@ int main(int argc, char *argv[]) {
 
   // Load file into ROM
   const char* filename = argv[1];
-  FILE* file = fopen(filename, "rb");
-  if (file == NULL) {
+  FILE* image = fopen(filename, "rb");
+  if (image == NULL) {
     error("Could not open file %s\n", filename);
     return EXIT_FAILURE;
   }
+  load_image(&state, image);
 
   return EXIT_SUCCESS;
 }
