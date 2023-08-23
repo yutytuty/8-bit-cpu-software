7ed39b441364ebd80fa98ee7281b29548e66b4cf
yutytuty <49990984+yutytuty@users.noreply.github.com>
2023-08-23 16:40:34 +0300

    Add initialization, instructions and register union

--- common/cpu.h
+++ common/cpu.h
@@ -10,9 +10,24 @@
 // TODO in design document, fill whole memory
 #define TOTAL_MEMORY_SIZE ROM_SIZE+RAM_SIZE+STCK_SIZE
 
+enum Instruction {
+  MOV  = 0x0,
+  ADD  = 0x1,
+  SUB  = 0x2,
+  PUSH = 0x3,
+  POP  = 0x4,
+  LOD  = 0x5,
+  STO  = 0x6,
+  JNZ  = 0x7,
+  HLT  = 0xF,
+};
+
 struct CPU {
-  struct Registers {
-    uint8_t ra, rb, rc, rxh, rxl, rf;
+  union {
+    struct {
+      uint8 ra, rb, rc, rxh, rxl, rf;
+    };
+    uint8 raw[6];
   } registers;
 
   union {
@@ -21,13 +36,17 @@ struct CPU {
       uint8 ram[RAM_SIZE];
       uint8 stck[STCK_SIZE];
     };
-
     uint8 raw[ROM_SIZE + RAM_SIZE + STCK_SIZE];
   } memory;
 
   uint16_t sp;
   uint16_t pc;
 };
+
+static void inline initialize_cpu(struct CPU* state) {
+  state->pc = 0;
+}
+
 
 static inline void poke(struct CPU* state, uint16 addr, uint8 value) {
   if (addr > TOTAL_MEMORY_SIZE) {
