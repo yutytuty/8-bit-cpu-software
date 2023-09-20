7e911e520977fd8977e786df958889c355f4ae9c
yutytuty <49990984+yutytuty@users.noreply.github.com>
2023-09-20 22:17:42 +0300

    8BC-12 Add basic main

---
+++ emu/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Yotam Yizhar on 20/09/2023.
+//
+
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
